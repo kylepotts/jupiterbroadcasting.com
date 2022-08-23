cc2bbc41981a3afadec38badda3098258008a41a
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2022-08-23 07:28:38 -0400

    - migrating most large variables to fixtures - updated teh cod based on new fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,71 @@
+from pathlib import Path
+from typing import Dict, List
+from pytest import fixture
 from playwright.sync_api import Page
 from urllib.parse import urlparse
 
 def base_url(page: Page):
     parsed_url = urlparse(page.url)
     return "{}://{}".format(parsed_url.scheme, parsed_url.netloc)
+
+@fixture
+def screenshot_dir() -> Path:
+    return Path('screenshots/')
+
+@fixture
+def expected_rss_feeds() -> List[Dict[str,str,]]:
+    return [
+        { 'href': 'http://feeds2.feedburner.com/JupiterBroadcasting', 'title': 'All Shows Feed - Audio'},
+        { 'href': 'http://feeds2.feedburner.com/AllJupiterVideos', 'title': 'All Shows Feed - Video'},
+        { 'href': 'https://coder.show/rss', 'title': 'Coder Radio'},
+        { 'href': 'https://extras.show/rss', 'title': 'Jupiter EXTRAS'},
+        { 'href': 'https://linuxactionnews.com/rss', 'title': 'Linux Action News'},
+        { 'href': 'https://linuxunplugged.com/rss', 'title': 'LINUX Unplugged'},
+        { 'href': 'https://www.officehours.hair/rss', 'title': 'Office Hours'},
+        { 'href': 'https://selfhosted.show/rss', 'title': 'Self-Hosted'}
+    ]
+
+@fixture
+def expected_dropdown_items(base_url) -> List[Dict[str,str]]:
+    return [
+        {'href': '/hosts', 'title': 'Hosts'},
+        {'href': '/guests', 'title': 'Guests'},
+        {'href': 'https://github.com/JupiterBroadcasting/', 'title': 'GitHub'},
+        {'href': 'https://jupiter.tube', 'title': 'Peertube'},
+        {'href': 'https://www.meetup.com/jupiterbroadcasting', 'title': 'Meetup'},
+        {'href': 'https://www.youtube.com/user/jupiterbroadcasting', 'title': 'YouTube'},
+        {'href': 'https://twitter.com/jupitersignal', 'title': 'Twitter'},
+        {'href': '{}/community/irc'.format(base_url), 'title': 'IRC'},
+        {'href': 'http://www.facebook.com/pages/Jupiter-Broadcasting/156241429615', 'title': 'Facebook'},
+        {'href': 'https://discord.com/invite/n49fgkp', 'title': 'Self-Hosted Discord'},
+        {'href': '{}/community/matrix'.format(base_url), 'title': 'Matrix'},
+        {'href': '{}/community/mumble'.format(base_url), 'title': 'Mumble'},
+        {'href': 'https://t.me/jupitertelegram', 'title': 'Telegram'},
+        {'href': '/show/coder-radio', 'title': 'Coder Radio'},
+        {'href': '/show/jupiter-extras', 'title': 'Jupiter EXTRAS'},
+        {'href': '/show/linux-action-news', 'title': 'Linux Action News'},
+        {'href': '/show/linux-unplugged', 'title': 'LINUX Unplugged'},
+        {'href': '/show/office-hours', 'title': 'Office Hours'},
+        {'href': '/show/self-hosted', 'title': 'Self-Hosted'},
+    ]
+
+@fixture
+def expected_dropdowns() -> List[Dict[str,str]]:
+    return [
+        {'title': 'Shows', 'href': '/show'},
+        {'title': 'People', 'href': "/people"},
+        {'title': 'Community', 'href': "/community"}
+    ]
+
+@fixture
+def expect_nav_items() -> List[Dict[str,str]]:
+    return [
+        {'title': 'Sponsors', 'href': '/sponsors'},
+        {'title': 'Live', 'href': 'https://jb-live.jupiterbroadcasting.net/'},
+        {'title': 'Calendar', 'href': '/calendar'},
+        {'title': 'Boost!', 'href': '/boost'},
+        {'title': 'Garage Sale', 'href': 'https://www.jupitergarage.com/'},
+        {'title': 'Membership', 'href': '/membership'},
+        {'title': 'Archive', 'href': '/archive'},
+        {'title': 'Contact', 'href': '/contact'},
+    ]
--- test/e2e/test_home.py
+++ test/e2e/test_home.py
@@ -1,10 +1,12 @@
 import re
+from pathlib import Path
+from typing import Dict, List
 from playwright.sync_api import Page, expect
 
-def test_homepage_screenshot(page: Page):
+def test_homepage_screenshot(page: Page, screenshot_dir: Path):
     page.goto("/")
     page.pause()
-    page.screenshot(path="screenshots/home.png", full_page=True)
+    page.screenshot(path=f"{screenshot_dir}/home.png", full_page=True)
 
 def test_homepage_has_logo(page: Page):
     page.goto("/")
@@ -24,48 +26,17 @@ def test_pagination(page: Page):
     first_card_second_page = page.locator('.card').nth(0).text_content
     assert first_card != first_card_second_page
 
-def test_rss_feeds(page: Page):
+def test_rss_feeds(page: Page, expected_rss_feeds: List[Dict[str,str]]):
     page.goto("/")
-    expected_rss_feeds = [
-        { 'href': 'http://feeds2.feedburner.com/JupiterBroadcasting', 'title': 'All Shows Feed - Audio'},
-        { 'href': 'http://feeds2.feedburner.com/AllJupiterVideos', 'title': 'All Shows Feed - Video'},
-        { 'href': 'https://coder.show/rss', 'title': 'Coder Radio'},
-        { 'href': 'https://extras.show/rss', 'title': 'Jupiter EXTRAS'},
-        { 'href': 'https://linuxactionnews.com/rss', 'title': 'Linux Action News'},
-        { 'href': 'https://linuxunplugged.com/rss', 'title': 'LINUX Unplugged'},
-        { 'href': 'https://www.officehours.hair/rss', 'title': 'Office Hours'},
-        { 'href': 'https://selfhosted.show/rss', 'title': 'Self-Hosted'}
-    ]
 
     for rss_feed in expected_rss_feeds:
         element = page.locator('#rss-feeds-menu > div > a[href^="{}"]'.format(rss_feed['href']))
         expect(element).to_contain_text(rss_feed['title'])
 
 
-def test_dropdowns(page: Page, base_url: str):
+def test_dropdowns(page: Page, expected_dropdown_items):
     page.goto("/")
 
-    expected_dropdown_items = [
-        {'href': '/hosts', 'title': 'Hosts'},
-        {'href': '/guests', 'title': 'Guests'},
-        {'href': 'https://github.com/JupiterBroadcasting/', 'title': 'GitHub'},
-        {'href': 'https://jupiter.tube', 'title': 'Peertube'},
-        {'href': 'https://www.meetup.com/jupiterbroadcasting', 'title': 'Meetup'},
-        {'href': 'https://www.youtube.com/user/jupiterbroadcasting', 'title': 'YouTube'},
-        {'href': 'https://twitter.com/jupitersignal', 'title': 'Twitter'},
-        {'href': '{}/community/irc'.format(base_url), 'title': 'IRC'},
-        {'href': 'http://www.facebook.com/pages/Jupiter-Broadcasting/156241429615', 'title': 'Facebook'},
-        {'href': 'https://discord.com/invite/n49fgkp', 'title': 'Self-Hosted Discord'},
-        {'href': '{}/community/matrix'.format(base_url), 'title': 'Matrix'},
-        {'href': '{}/community/mumble'.format(base_url), 'title': 'Mumble'},
-        {'href': 'https://t.me/jupitertelegram', 'title': 'Telegram'},
-        {'href': '/show/coder-radio', 'title': 'Coder Radio'},
-        {'href': '/show/jupiter-extras', 'title': 'Jupiter EXTRAS'},
-        {'href': '/show/linux-action-news', 'title': 'Linux Action News'},
-        {'href': '/show/linux-unplugged', 'title': 'LINUX Unplugged'},
-        {'href': '/show/office-hours', 'title': 'Office Hours'},
-        {'href': '/show/self-hosted', 'title': 'Self-Hosted'},
-    ]
     for dropdown_item in expected_dropdown_items:
         selector = '.navbar-item > .navbar-dropdown > a[href^="{}"]'.format(dropdown_item['href'])
         element = page.locator(selector)
@@ -73,24 +44,7 @@ def test_dropdowns(page: Page, base_url: str):
     
 
 
-def test_nav(page: Page):
-    expected_dropdowns = [
-        {'title': 'Shows', 'href': '/show'},
-        {'title': 'People', 'href': "/people"},
-        {'title': 'Community', 'href': "/community"}
-    ]
-
-    expect_nav_items = [
-        {'title': 'Sponsors', 'href': '/sponsors'},
-        {'title': 'Live', 'href': 'https://jb-live.jupiterbroadcasting.net/'},
-        {'title': 'Calendar', 'href': '/calendar'},
-        {'title': 'Boost!', 'href': '/boost'},
-        {'title': 'Garage Sale', 'href': 'https://www.jupitergarage.com/'},
-        {'title': 'Membership', 'href': '/membership'},
-        {'title': 'Archive', 'href': '/archive'},
-        {'title': 'Contact', 'href': '/contact'},
-    ]
-
+def test_nav(page: Page, expected_dropdowns, expect_nav_items):
 
     page.goto("/")
     nav = page.locator('#mainnavigation')
@@ -110,12 +64,4 @@ def test_nav(page: Page):
     
     nav_image = page.locator('.navbar-brand > a > img')
 
-
     expect(nav_image.nth(0)).to_be_visible()
-
-
-
-
-
-
-    
